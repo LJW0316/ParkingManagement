bb359841171f96233b07f8b91dca975b9aa13d7e
Pym <278290749@qq.com>
2022-05-25 22:27:38 +0800

    整合

--- src/components/HeaderBar.vue
+++ src/components/HeaderBar.vue
@@ -10,20 +10,22 @@
     <div style="width: 200px;padding-left: 30px;font-weight: bold;color: #ffd04b">教务管理系统</div>
     <div style="flex: 1px"></div>
     <div style="width: 100px">
-      <el-dropdown>
-        <el-button type="primary" text>
-          {{ name }}
-          <el-icon class="el-icon--right">
-            <arrow-down/>
-          </el-icon>
-        </el-button>
-        <template #dropdown>
-          <el-dropdown-menu>
-            <el-dropdown-item>个人信息</el-dropdown-item>
-            <el-dropdown-item @click="logout">退出登录</el-dropdown-item>
-          </el-dropdown-menu>
-        </template>
-      </el-dropdown>
+      <div style="width: 100px">
+        <el-dropdown>
+          <el-button type="primary" text>
+            {{ name }}
+            <el-icon class="el-icon--right">
+              <arrow-down/>
+            </el-icon>
+          </el-button>
+          <template #dropdown>
+            <el-dropdown-menu>
+              <el-dropdown-item>个人信息</el-dropdown-item>
+              <el-dropdown-item @click="logout">退出登录</el-dropdown-item>
+            </el-dropdown-menu>
+          </template>
+        </el-dropdown>
+      </div>
     </div>
   </el-menu>
 </template>

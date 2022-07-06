5b3012af6b5af508dbf4f7ce0b5840ec518c179f
LJW0316 <lijingwei010316@163.com>
2022-07-06 21:25:17 +0800

    添加了分页插件

--- ParkingSystem/src/main/java/com/sixzerofour/parkingsystem/config/MyBatisPlusConfig.java
+++ ParkingSystem/src/main/java/com/sixzerofour/parkingsystem/config/MyBatisPlusConfig.java
@@ -1,6 +1,10 @@
 package com.sixzerofour.parkingsystem.config;
 
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -8,4 +12,14 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @MapperScan("com.sixzerofour.parkingsystem.mapper")
 @EnableTransactionManagement//事务管理
 public class MyBatisPlusConfig {
+    /**
+     * 分页插件
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+
 }

b0dd1ea69fae8cd0a0dd0b1106a392a4a2762bb0
Pym <278290749@qq.com>
2022-08-12 22:50:07 +0800

    删除header上的无用功能

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,22 +8,6 @@
     <div style="width: 200px;padding-left: 30px;font-weight: bold;color: whitesmoke">停车场管理系统</div>
     <div style="flex: 1px"></div>
     <div style="width: 100px">
-      <div style="width: 100px">
-        <el-dropdown>
-          <el-button type="primary" text>
-            {{ name }}
-            <el-icon class="el-icon--right">
-              <arrow-down/>
-            </el-icon>
-          </el-button>
-          <template #dropdown>
-            <el-dropdown-menu>
-              <el-dropdown-item @click="openDialog">个人信息</el-dropdown-item>
-              <el-dropdown-item @click="logout">退出登录</el-dropdown-item>
-            </el-dropdown-menu>
-          </template>
-        </el-dropdown>
-      </div>
     </div>
   </div>
 </template>
